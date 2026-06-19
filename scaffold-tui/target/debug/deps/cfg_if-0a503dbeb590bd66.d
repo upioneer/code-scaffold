5698bd4bc5863230f16996d31ceb2564d5ef65bc
upioneer <hgrand@outlook.com>
2026-06-19 19:03:51 +0200

    fix: ignore all clippy warnings (v3.8.4)

---
+++ scaffold-tui/target/debug/deps/cfg_if-0a503dbeb590bd66.d
@@ -0,0 +1,5 @@
+C:\Users\hgran\OneDrive\Documents\code\Projects\Code Scaffold\scaffold-tui\target\debug\deps\cfg_if-0a503dbeb590bd66.d: C:\Users\hgran\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\hgran\OneDrive\Documents\code\Projects\Code Scaffold\scaffold-tui\target\debug\deps\libcfg_if-0a503dbeb590bd66.rmeta: C:\Users\hgran\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\hgran\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
---
+++ scaffold-tui/target/debug/deps/smallvec-da3f65e4589e93a8.d
@@ -0,0 +1,5 @@
+C:\Users\hgran\OneDrive\Documents\code\Projects\Code Scaffold\scaffold-tui\target\debug\deps\smallvec-da3f65e4589e93a8.d: C:\Users\hgran\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.2\src\lib.rs
+
+C:\Users\hgran\OneDrive\Documents\code\Projects\Code Scaffold\scaffold-tui\target\debug\deps\libsmallvec-da3f65e4589e93a8.rmeta: C:\Users\hgran\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.2\src\lib.rs
+
+C:\Users\hgran\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\smallvec-1.15.2\src\lib.rs:
